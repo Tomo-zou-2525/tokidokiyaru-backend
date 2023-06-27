d71e60011349d0ba34f5e01674d4a590016bd524
sakon-ziwave <ryo.development0320@gmail.com>
2023-06-27 14:49:20 +0000

    feature: PostgreSQL用の設定に変更

--- src/sql_app/database.py
+++ src/sql_app/database.py
@@ -1,12 +1,23 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+load_dotenv()
 
+psql_user = os.getenv('POSTGRES_USER')
+psql_password = os.getenv('POSTGRES_PASSWORD')
+psql_server = os.getenv('POSTGRES_SERVER')
+psql_port = os.getenv('POSTGRES_PORT')
+psql_db = os.getenv('POSTGRES_DB')
+
+SQLALCHEMY_DATABASE_URL = "postgresql://{0}:{1}@{2}:{3}/{4}".format(
+    psql_user, psql_password, psql_server, psql_port, psql_db
+)
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={
-        "check_same_thread": False}  # SQLite specific
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
