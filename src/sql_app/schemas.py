ad6976c03b7ff570641afde2c0e6438a09c9e35d
sakon-ziwave <ryo.development0320@gmail.com>
2023-07-02 15:57:12 +0000

    fix: 一時的にpasswordの制限をハッシュ値用に広げる

--- src/sql_app/schemas.py
+++ src/sql_app/schemas.py
@@ -6,7 +6,8 @@ from pydantic import BaseModel, Field
 class UserCreate(BaseModel):
     name: str = Field(max_length=12)
     email: str
-    password: str = Field(max_length=12)
+    # FIXME: 12文字までの制限にする（今はハッシュ化した12文字以上の値をreturnしている）
+    password: str = Field(max_length=120)
 
 
 class User(UserCreate):
