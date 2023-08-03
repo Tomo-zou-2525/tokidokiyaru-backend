0e8401b9e4e474dc57e21806b9fb2496a6da67ff
RyoSakon001 <ryo.development0320@gmail.com>
2023-08-03 03:22:19 +0000

    feature: Readのテスト実装

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,20 @@ def test_sample():
     response = client.get("/sample")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+# Read
+
+
+def test_read_users():
+    response = client.get("/users")
+    assert response.status_code == 200
+
+
+def test_read_tasks():
+    response = client.get("/tasks")
+    assert response.status_code == 200
+
+
+def test_read_run_dates():
+    response = client.get("/rundates")
+    assert response.status_code == 200
