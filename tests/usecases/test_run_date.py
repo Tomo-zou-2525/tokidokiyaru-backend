7c5c574c242a25780bfc76f683d6addfe7fad777
RyoSakon001 <ryo.development0320@gmail.com>
2023-08-29 15:22:45 +0900

    微調整

--- tests/usecases/test_run_date.py
+++ tests/usecases/test_run_date.py
@@ -1,6 +1,5 @@
 import random
 
-# from app.schemas.run_date import RunDateCreate
 from tests.test_main import client
 
 
@@ -13,7 +12,7 @@ test_rand_str = "test" + str(random.randint(0, 10000000))
 
 
 def test_create_run_date():
-    json_data = dict(task_id=1)
+    json_data = dict(task_id=2)
     response = client.post("/rundates", json=json_data)
     assert response.status_code == 200
     assert response.json()["date"]  # datetimeは存在するかだけ確認
