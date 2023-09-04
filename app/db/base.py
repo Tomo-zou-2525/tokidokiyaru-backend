1f32063379df07329e671bcad548baa8f605b614
RyoSakon001 <ryo.development0320@gmail.com>
2023-09-05 01:11:23 +0900

    alembicのBaseが抜けていたので追加

--- app/db/base.py
+++ app/db/base.py
@@ -1,6 +1,7 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 
+from app.db.base_class import Base  # noqa
 from app.models.run_date import RunDate  # noqa
 from app.models.task import Task  # noqa
 from app.models.user import User  # noqa
