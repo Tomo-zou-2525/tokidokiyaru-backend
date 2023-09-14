47ec1eed53afc860ea157a46da1eb2e12be4e58d
RyoSakon001 <ryo.development0320@gmail.com>
2023-09-14 20:37:55 +0900

    CORSの設定

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.usecases.run_date import router as run_date_router
 from app.usecases.task import router as task_router
@@ -8,6 +9,14 @@ app = FastAPI()
 app.include_router(user_router)
 app.include_router(task_router)
 app.include_router(run_date_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/sample")
