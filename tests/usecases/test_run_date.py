4437cb98eed71c203b4f23127b47d01322b1f699
RyoSakon001 <ryo.development0320@gmail.com>
2023-09-15 08:39:03 +0900

    使わなくなったエンドポイントのテストを削除

--- tests/usecases/test_run_date.py
+++ tests/usecases/test_run_date.py
@@ -1,11 +1,6 @@
 from tests.test_main import client
 
 
-def test_read_run_dates():
-    response = client.get("/rundates")
-    assert response.status_code == 200
-
-
 def test_create_run_date():
     json_data = dict(task_id=2)
     response = client.post("/rundates", json=json_data)
