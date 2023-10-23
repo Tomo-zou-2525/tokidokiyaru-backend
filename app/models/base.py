4dd8a829d396ea933ccd01c153b9d417099740d8
RyoSakon001 <ryo.development0320@gmail.com>
2023-10-23 15:32:56 +0900

    createdAt, updatedAtは不要と判断し削除

--- app/models/base.py
+++ app/models/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import DATETIME, Integer, func
+from sqlalchemy import Integer
 from sqlalchemy.orm import mapped_column
 
 
@@ -6,14 +6,4 @@ class ModelBase:
     id = mapped_column(Integer, primary_key=True, sort_order=-10)
     order_num = mapped_column(
         Integer, nullable=False, server_default="0", sort_order=97
-    )
-    created_at = mapped_column(
-        DATETIME, nullable=False, server_default=func.now(), sort_order=98
-    )
-    updated_at = mapped_column(
-        DATETIME,
-        nullable=False,
-        server_default=func.now(),
-        onupdate=func.now(),
-        sort_order=99,
     )
