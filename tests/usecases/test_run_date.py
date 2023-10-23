eadb750e0269b3b5e1c964ee9aba31463bce866a
RyoSakon001 <ryo.development0320@gmail.com>
2023-10-23 17:11:49 +0900

    実施日のテストを修正

--- tests/usecases/test_run_date.py
+++ tests/usecases/test_run_date.py
@@ -1,8 +1,35 @@
 from tests.test_main import client
 
+endpoint = "rundates"
 
-def test_create_run_date():
-    json_data = dict(task_id=2)
-    response = client.post("/rundates", json=json_data)
-    assert response.status_code == 200
-    assert response.json()["date"]  # datetimeは存在するかだけ確認
+
+def test_create_rundate():
+    # ユーザー作成
+    response = client.post(
+        "/users/",
+        json={"name": "テストユーザー３",
+              "email": "sample3@example.com", "password": "test3"},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert "id" in data
+    user_id = data["id"]
+    # タスク作成
+    response = client.post(
+        "/tasks/",
+        json={"userId": user_id, "name": "テストタスク"},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert "id" in data
+    task_id = data["id"]
+    # 実施日作成
+    response = client.post(
+        f"/{endpoint}/",
+        json={"taskId": task_id},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert "id" in data
+    assert data["taskId"] == task_id
+    assert "runAt" in data
