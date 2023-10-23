3f163aa25ac4fddd9b1989727c4a1da75582f91e
RyoSakon001 <ryo.development0320@gmail.com>
2023-10-23 15:51:02 +0900

    SQLiteでテストを実行することで、MySQLのDBを汚さないようにする

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,34 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
+from app.db.base_class import Base
+from app.db.session import get_db
 from app.main import app
+
+SQLALCHEMY_DATABASE_URL = "sqlite://"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
