085e7c090fe2d5aba0ffaff594f0e6715bb69f61
RyoSakon001 <ryo.development0320@gmail.com>
2023-10-23 17:00:26 +0900

    タスクのテストを修正

--- tests/usecases/test_task.py
+++ tests/usecases/test_task.py
@@ -1,26 +1,31 @@
-from random import randint
-
-from faker import Faker
-
-from app.schemas.task import TaskCreate
 from tests.test_main import client
 
-fake = Faker('ja_JP')
-
-
-def test_read_tasks():
-    response = client.get("/tasks")
-    assert response.status_code == 200
+endpoint = "tasks"
 
 
 def test_create_task():
-    task = TaskCreate(
-        user_id=1,
-        name=fake.job() + 'の勉強',
-        order=randint(0, 10000000)
+    response = client.post(
+        "/users/",
+        json={"name": "テストユーザー２",
+              "email": "sample2@example.com", "password": "test2"},
     )
-    response = client.post("/tasks", json=task.dict())
-    assert response.status_code == 200
-    assert response.json()["user_id"] == task.user_id
-    assert response.json()["name"] == task.name
-    assert response.json()["order"] == task.order
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert "id" in data
+    user_id = data["id"]
+
+    response = client.post(
+        f"/{endpoint}/",
+        json={"userId": user_id, "name": "テストタスク"},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert "id" in data
+    task_id = data["id"]
+
+    response = client.get(f"/{endpoint}/{task_id}")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["id"] == task_id
+    assert data["userId"] == user_id
+    assert data["name"] == "テストタスク"
