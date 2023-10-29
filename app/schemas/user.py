6e95a3687dac629c38d29a9ae8ae9294b1abc606
RyoSakon001 <ryo.development0320@gmail.com>
2023-10-30 01:19:53 +0900

    UserのスキーマでNone許可を追加

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -4,7 +4,7 @@ from pydantic import Field, ConfigDict
 
 class UserSchemaBase(BaseSchema):
     name: str = Field(max_length=12, examples=["テストユーザー"])
-    email: str | None = Field(max_length=255, examples=["sample@example.com"])
+    email: str = Field(max_length=255, examples=["sample@example.com"])
 
     model_config = ConfigDict(from_attributes=True)
 
@@ -14,9 +14,10 @@ class UserResponse(UserSchemaBase):
 
 
 class UserCreate(UserSchemaBase):
-    password: str | None = Field(max_length=120)
+    password: str | None = Field(None, max_length=120)
 
 
 class UserUpdate(UserSchemaBase):
+    name: str | None = Field(None, max_length=12, examples=["テストユーザー"])
     email: str | None = Field(None, max_length=255, examples=["sample@example.com"])
     password: str | None = Field(None, max_length=120)
