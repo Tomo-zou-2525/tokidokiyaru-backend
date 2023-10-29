0962147244953a7818849cdcec5a7ae24cc5bac1
RyoSakon001 <ryo.development0320@gmail.com>
2023-10-29 22:40:28 +0900

    TaskResponseにDonesを含める

--- app/usecases/done.py
+++ app/usecases/done.py
@@ -1,54 +1,23 @@
-from typing import List
-
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.db.session import get_db
-from app.models.done import Done
+from app.models.task import Task
 from app.repositories.done import done
 from app.schemas.done import (
     DoneCreate,
-    DoneResponse,
-    DoneUpdate,
 )
+from app.schemas.task import TaskResponse
 
 
 router = APIRouter()
 
 
-@router.get("/{id}", response_model=DoneResponse)
-def get_done(id: int, db: Session = Depends(get_db)) -> Done:
-    done_data = done.get(id=id, db=db)
-
-    return done_data
-
-
-@router.get("", response_model=List[DoneResponse])
-def get_done_list(db: Session = Depends(get_db)) -> List[Done]:
-    done_data = done.get_list(db=db)
-
-    return done_data
-
-
-@router.post("", response_model=DoneResponse)
-def create_done(data_in: DoneCreate, db: Session = Depends(get_db)) -> Done:
+@router.post("", response_model=TaskResponse)
+def create_done(data_in: DoneCreate, db: Session = Depends(get_db)) -> Task:
     return done.create(db=db, obj_in=data_in)
 
 
-@router.post("/rand", response_model=DoneResponse)
-def create_rand_done(db: Session = Depends(get_db)) -> Done:
-    data_in = DoneCreate(task_id=1)
-
-    return done.create(db=db, obj_in=data_in)
-
-
-@router.put("/{id}", response_model=DoneResponse)
-def update_done(
-    id: int, data_in: DoneUpdate, db: Session = Depends(get_db)
-) -> Done:
-    return done.update(db=db, id=id, obj_in=data_in)
-
-
-@router.delete("/{id}", response_model=DoneResponse)
-def hard_delete_done(id: int, db: Session = Depends(get_db)) -> Done:
+@router.delete("/{id}", response_model=TaskResponse)
+def hard_delete_done(id: int, db: Session = Depends(get_db)) -> Task:
     return done.hard_delete(db=db, id=id)
